9a668f41ea9ee1ce3476ca22a64157aa4eaf7fd1
Troy mangrum <tmmangrum@gmail.com>
2020-05-08 22:41:10 -0500

    added some additional events

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// Your code goes here
+
 //------- Fun Bus Logo, Mouse Enter & Mouse Leave------
 //-----------------------------------------------------
 const funBusLogo = document.querySelector(".logo-heading");
@@ -21,5 +21,61 @@ const greenBlock = document.querySelector(".intro img");
     greenBlock.addEventListener("dblclick", (event) => {
     greenBlock.src = "img/destination.jpg";
   });
+//-----------------------------------------------------
+//-----------------------------------------------------
+
+
+
+//-----------Body Color Change on click----------------
+//-----------------------------------------------------
+const body = document.querySelector("body");
+body.addEventListener("click", () => {
+  body.style.backgroundColor="green";
+});
+const navColor = document.querySelector("header");
+navColor.addEventListener("click", () => {
+  navColor.style.backgroundColor="seagreen";
+  navColor.style.color="white";
+});
+//-----------------------------------------------------
+//-----------------------------------------------------
+
+
+
+
+//-----------------Resize Changes Image----------------
+//-----------------------------------------------------
+const imgSize = document.querySelector(".content-section img")
+window.addEventListener("resize", () => {
+    imgSize.src = "img/fun.jpg"
+});
+//-----------------------------------------------------
+//-----------------------------------------------------
+
+
+
+
+//---------------------Scroll--------------------------
+//-----------------------------------------------------
+window.addEventListener("scroll", () => {
+    const scrolled = window.scrollY; 
+
+    console.log(scrolled);
+});
+//-----------------------------------------------------
+//-----------------------------------------------------
+
+
+
+//--------------------For Each-------------------------
+//-----------------------------------------------------
+
+const navFontColor = document.querySelectorAll(".nav-link");
+navFontColor.forEach(title => {
+    title.addEventListener('click', (event) => {
+        event.target.style.color = 'white';
+    });
+});
+
 //-----------------------------------------------------
 //-----------------------------------------------------
