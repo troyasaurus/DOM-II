0acd17c4561a332d268a7bfb9e9e16959010f4b2
Troy mangrum <tmmangrum@gmail.com>
2020-05-08 20:08:03 -0500

    added an event that changes the logo to BUS FUN when the mouse is over it, and back to Fun Bus when it's not

--- js/index.js
+++ js/index.js
@@ -1,2 +1,21 @@
 // Your code goes here
-// just starting
+//------- Fun Bus Logo, Mouse Enter & Mouse Leave------
+//-----------------------------------------------------
+const funBusLogo = document.querySelector(".logo-heading");
+funBusLogo.addEventListener("mouseover", (event) => {
+    funBusLogo.textContent = "BUS FUN";
+    funBusLogo.style.transition = "all 0.3s";
+   });
+   funBusLogo.addEventListener("mouseleave", (event) => {
+    funBusLogo.textContent = "Fun Bus";
+    funBusLogo.style.transition = "all 0.3s";
+   });
+   //--------------------------------------------------
+//-----------------------------------------------------
+
+
+const bottomPage = document.querySelector("body");
+bottomPage.addEventListener("keydown", (event) => {
+    bottomPage
+    bottomPage.style.transition = "all 0.3s";
+   });
